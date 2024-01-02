07a2b3bbcca4ef15415f9bb3a094486c6ec03ede
BadHard <BadHard101@yandex.ru>
2024-01-02 20:31:32 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
