c6d1b0135f36bf2a3553fb81818e99d10aea87c6
BadHard <BadHard101@yandex.ru>
2024-01-05 08:00:06 +0300

    initial

--- src/main/java/org/example/Main1.java
+++ src/main/java/org/example/Main1.java
@@ -1,10 +1,9 @@
 package org.example;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Main {
+public class Main1 {
     public static boolean containsNearbyDuplicate(int[] nums, int k) {
         Set<Integer> set = new HashSet<>();
 
@@ -13,10 +12,12 @@ public class Main {
             return nums.length != set.size();
         } else {
             for (int i = 0; i < k; i++) {
-                if (!set.add(nums[i])) return true;
+                if (set.contains(nums[i])) return true;
+                set.add(nums[i]);
             }
             for (int i = k; i < nums.length; i++) {
-                if (!set.add(nums[i])) return true;
+                if (set.contains(nums[i])) return true;
+                set.add(nums[i]);
                 set.remove(nums[i-k]);
             }
         }
